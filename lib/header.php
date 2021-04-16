aa77c5527a26769365f4a6f7cb66428bdb87b23e
iammradioactive <42364197+iammradioactive@users.noreply.github.com>
2021-04-16 13:33:49 +0100

    Update and rename ..header.php to header.php

--- lib/header.php
+++ lib/header.php
@@ -4,6 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to SNG : Hospital for the Ignorant</title>
+    <title>Welcome to Logixx : Hub for Tech enthusiasts</title>
 </head>
 <body>
